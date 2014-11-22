b796b1ee179d1fa528a501c7c806628a19b38159
Seth Deckard <seth@deckard.us>
2014-11-22 13:05:33 -0600

    refactor alert

--- lib/gull/alert.rb
+++ lib/gull/alert.rb
@@ -3,8 +3,9 @@ require 'nokogiri'
 
 module Gull
   class Alert
-    attr_accessor :id, :title, :summary, :link, :alert_type, :polygon, :area, :effective_at, 
-      :expires_at, :updated_at, :published_at, :urgency, :severity, :certainty, :geocode, :vtec
+    attr_accessor :id, :title, :summary, :link, :alert_type, :polygon, :area,
+                  :effective_at, :expires_at, :updated_at, :published_at,
+                  :urgency, :severity, :certainty, :geocode, :vtec
 
     def initialize
       self.geocode = Geocode.new
@@ -12,19 +13,13 @@ module Gull
 
     def self.fetch
       client = HTTPClient.new
-      response = client.get_content "http://alerts.weather.gov/cap/us.php?x=0"
+      response = client.get_content 'http://alerts.weather.gov/cap/us.php?x=0'
       document = Nokogiri::XML response
-      self.process document.css('feed/entry')
+      process document.css('feed/entry')
     end
 
-    def parse element
-      self.id = element.css('id').inner_text
-      self.title = element.css('title').inner_text
-      self.summary = element.css('summary').inner_text
-      self.link = element.css('link').first.attributes["href"].value
-      self.alert_type = element.xpath('cap:event').inner_text
-      self.area = element.xpath('cap:areaDesc').inner_text
-
+    def parse(element)
+      parse_core_attributes element
       parse_times element
       parse_categories element
 
@@ -35,7 +30,7 @@ module Gull
 
     private
 
-    def self.process entries
+    def self.process(entries)
       alerts = []
       entries.each do |entry|
         alerts.push create_instance entry
@@ -44,44 +39,52 @@ module Gull
       alerts
     end
 
-    def self.create_instance entry
+    def self.create_instance(entry)
       alert = Alert.new
       alert.parse entry
       alert
     end
 
-    def parse_times element
+    def parse_core_attributes(element)
+      self.id = element.css('id').inner_text
+      self.title = element.css('title').inner_text
+      self.summary = element.css('summary').inner_text
+      self.link = element.css('link').first.attributes['href'].value
+      self.alert_type = element.xpath('cap:event').inner_text
+      self.area = element.xpath('cap:areaDesc').inner_text
+    end
+
+    def parse_times(element)
       self.updated_at = Time.parse(element.css('updated').inner_text).utc
       self.published_at = Time.parse(element.css('published').inner_text).utc
-      self.effective_at = Time.parse(element.xpath('cap:effective').inner_text).utc
+      self.effective_at = Time.parse(element.xpath('cap:effective').inner_text)
+        .utc
       self.expires_at = Time.parse(element.xpath('cap:expires').inner_text).utc
     end
 
-    def parse_categories element
+    def parse_categories(element)
       self.urgency = code_to_symbol element.xpath('cap:urgency').inner_text
       self.severity = code_to_symbol element.xpath('cap:severity').inner_text
       self.certainty = code_to_symbol element.xpath('cap:certainty').inner_text
     end
 
-    def parse_polygon text
-      unless text.empty?
-        self.polygon = Polygon.new text
-      end
+    def parse_polygon(text)
+      return if text.empty?
+      self.polygon = Polygon.new text
     end
 
-    def parse_geocode element
-      self.geocode.fips6 = element.children.css('value').first.inner_text
-      self.geocode.ugc = element.children.css('value').last.inner_text
+    def parse_geocode(element)
+      geocode.fips6 = element.children.css('value').first.inner_text
+      geocode.ugc = element.children.css('value').last.inner_text
     end
 
-    def parse_vtec element
+    def parse_vtec(element)
       value = element.children.css('value').inner_text
       self.vtec = value.empty? ? nil : value
     end
 
-    def code_to_symbol code
-      code.gsub(' ','_').downcase.to_sym
+    def code_to_symbol(code)
+      code.gsub(' ', '_').downcase.to_sym
     end
-
   end
 end
