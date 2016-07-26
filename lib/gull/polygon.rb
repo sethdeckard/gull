5c346918b0101bd531ba4d7a114ea73642a78169
Seth Deckard <seth@deckard.me>
2016-07-26 08:59:44 -0500

    remove centroid

--- lib/gull/polygon.rb
+++ lib/gull/polygon.rb
@@ -8,28 +8,6 @@ module Gull
       end
     end
 
-    def centroid
-      low_x = 0
-      low_y = 0
-      high_x = 0
-      high_y = 0
-
-      coordinates.each do |pair|
-        x_bounds = bounds pair.first, low_x, high_x
-        low_x = x_bounds.first
-        high_x = x_bounds.last
-
-        y_bounds = bounds pair.last, low_y, high_y
-        low_y = y_bounds.first
-        high_y = y_bounds.last
-      end
-
-      center_x = low_x + ((high_x - low_x) / 2)
-      center_y = low_y + ((high_y - low_y) / 2)
-
-      [center_x, center_y]
-    end
-
     def image_url(api_key, options = {})
       options = {
         width: 640,
@@ -59,16 +37,6 @@ module Gull
     end
 
     private
-
-    def bounds(point, low, high)
-      if point < low || low == 0
-        low = point
-      elsif point > high || high == 0
-        high = point
-      end
-
-      [low, high]
-    end
 
     def coordinates_piped
       coordinates.collect { |pair| pair.join ',' }.join '|'
